036c1c8d30d6e37a98d85bec7f38e01a62ea23fc
Lunaticfrg0 <emilio3scano@gmail.com>
2021-03-02 17:18:47 -0400

    base api get methods with mock data

--- CRUDAppBackend/Controllers/UsersController.cs
+++ CRUDAppBackend/Controllers/UsersController.cs
@@ -1,15 +1,35 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CRUDAppBackend.UserData;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace CRUDAppBackend.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
     public class UsersController : ControllerBase
     {
+        private IUserData _userData;
+        public UsersController(IUserData userData)
+        {
+            _userData = userData;
+        }
+        [HttpGet]
+        [Route("api/[controller]")]
+        public IActionResult GetUsers()
+        {
+            return Ok(_userData.GetUsers());
+        }
+        [HttpGet]
+        [Route("api/[controller]/{id}")]
+        public IActionResult GetUser(int id)
+        {
+            var usertmp = _userData.GetUser(id);
+            if (usertmp != null)
+            {
+               return Ok(usertmp);
+            }
+            return NotFound($"User with id: {id} not found!");
+        }
+
     }
 }
---
+++ CRUDAppBackend/UserData/MockUserData.cs
@@ -0,0 +1,49 @@
+﻿using CRUDAppBackend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUDAppBackend.UserData
+{
+    public class MockUserData : IUserData
+    {
+        private List<User> users = new List<User>()
+        {
+            new User()
+            {
+                Id = 1,
+                Name = "Emilio Escano"
+            },
+            new User()
+            {
+                Id = 2,
+                Name = "Lia Escano"
+            }
+        };
+        public User AddUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetUser(int id)
+        {
+            return users.SingleOrDefault(x => x.Id == id);
+        }
+
+        public List<User> GetUsers()
+        {
+            return users;
+        }
+
+        public User UpdateUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
