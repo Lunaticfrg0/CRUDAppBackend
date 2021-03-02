cb1f9fcec9114d39c4fc67b8ba945e39aa13c331
Lunaticfrg0 <emilio3scano@gmail.com>
2021-03-02 17:17:24 -0400

    Create Mock user Data interface for testing purposes

---
+++ CRUDAppBackend/UserData/IUserData.cs
@@ -0,0 +1,22 @@
+﻿using CRUDAppBackend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUDAppBackend.UserData
+{
+    public interface IUserData
+    {
+        List<User> GetUsers();
+
+        User GetUser(int id);
+
+        User AddUser(User user);
+
+        void DeleteUser(User user);
+
+        User UpdateUser(User user);
+            
+    }
+}
